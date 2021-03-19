541ba82bca0f21c830dca52a84f77cafef348725
nguyenlongit95 <nguyenlongit95@gmail.com>
2021-03-19 16:43:24 +0700

    update base admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            \App\Repositories\Paygates\PaygateRepositoryInterface::class,
+            \App\Repositories\Paygates\PaygateEloquentRepository::class
+        );
+        $this->app->bind(
+            \App\Repositories\Widgets\WidgetRepositoryInterface::class,
+            \App\Repositories\Widgets\WidgetEloquentRepository::class
+        );
+        $this->app->bind(
+            \App\Repositories\Users\UserRepositoryinterface::class,
+            \App\Repositories\Users\UserEloquentRepository::class
+        );
+        $this->app->bind(
+            \App\Repositories\Menus\MenusRepositoryInterface::class,
+            \App\Repositories\Menus\MenusEloquentRepository::class
+        );
     }
 
     /**
